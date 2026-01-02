5e596aaee727470d522a4e9fda601ad634d4fd61
Andrii Chychkan <mywork1728@gmail>
2026-01-02 17:56:12 +0200

    added authentication

--- cinema/views.py
+++ cinema/views.py
@@ -2,7 +2,9 @@ from datetime import datetime
 
 from django.db.models import F, Count
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 
